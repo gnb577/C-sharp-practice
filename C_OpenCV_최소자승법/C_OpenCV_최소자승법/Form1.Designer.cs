0b72677c7bd920e60e853dfc523fab9c64383180
gnb577 <73813639+gnb577@users.noreply.github.com>
2021-12-10 03:02:04 +0900

    Add files via upload

---
+++ C_OpenCV_최소자승법/C_OpenCV_최소자승법/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace C_OpenCV_최소자승법
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.winChartViewer1 = new ChartDirector.WinChartViewer();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.winChartViewer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(28, 42);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(566, 484);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(1257, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sampling Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(1257, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox1.Location = new System.Drawing.Point(1575, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(293, 81);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox2.Location = new System.Drawing.Point(1575, 192);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(293, 334);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 564);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 85);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 564);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 85);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // winChartViewer1
+            // 
+            this.winChartViewer1.Location = new System.Drawing.Point(632, 42);
+            this.winChartViewer1.Name = "winChartViewer1";
+            this.winChartViewer1.Size = new System.Drawing.Size(489, 484);
+            this.winChartViewer1.TabIndex = 8;
+            this.winChartViewer1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(645, 564);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 85);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox3.Location = new System.Drawing.Point(1575, 568);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(293, 81);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(1257, 571);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(303, 32);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Rans_samp_count";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox4.Location = new System.Drawing.Point(1575, 682);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(293, 81);
+            this.richTextBox4.TabIndex = 12;
+            this.richTextBox4.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(1257, 682);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 32);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Threshold";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1966, 1056);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.winChartViewer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.winChartViewer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private ChartDirector.WinChartViewer winChartViewer1;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ C_OpenCV_최소자승법/C_OpenCV_최소자승법/Form1.cs
@@ -0,0 +1,428 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using OpenCvSharp.Util;
+using System.Drawing.Drawing2D;
+using ChartDirector;
+using OpenCvSharp.ML;
+using System.Xml.Schema;
+using System.Runtime.CompilerServices;
+
+
+namespace C_OpenCV_최소자승법
+{
+    public partial class Form1 : Form
+    {
+        
+
+        Random r = new Random();
+        int i_value;
+//------------------------------------------------------------------------------- 공유
+        double sum_a = 0;
+        double sum_b = 0;
+
+        double[,] MatrixA;
+        double[,] MatrixB;
+        double[,] Pinv_MatrixA;
+        double[,] Matrix_ab;
+        double[,] numx;
+        double[,] numy;
+
+        Mat d_MatrixA, d_MatrixB, d_Pinv_MatrixA,d_Matrix_ab,d_numx,d_numy;
+ //------------------------------------------------------------------------------- 1번 2차원 
+        double[,] three_MatrixA;
+        double[,] three_Matrix_abcd;
+        double[,] s;
+        double[,] u;
+        double[,] v;
+        Mat d_three_MatrixA, d_three_Matrix_abcd, S, U, V;
+
+        //------------------------------------------------------------------------------- 2번 3차원
+     
+        //-------------------------------------------------------------------------------- 3번 Ransac 임시
+
+        double modelx;
+        double modely;
+        double modelsx;
+        double modelsy;
+
+        double max_rotation_count = 0;
+        int total_data_count = 0;
+        int sample_data_count = 0;
+        int inliers_count = 0;
+
+        double threshold;
+
+        int N = 3;
+        double cost = 0;
+        double max_cost = 0;
+
+        OpenCvSharp.Point[] total_data;
+        OpenCvSharp.Point[] sample_data;
+        OpenCvSharp.Point[] inliers;
+
+        Mat MatrixA3;
+        Mat MatrixB3;
+        Mat Matrix_abc3;
+        Mat Matrix_PinvA3;
+
+        double [,]ma3;
+        double [,]mb3;
+        double [,]mabc3;
+        double [,]mpa3;
+
+        double s_a;
+        double s_b;
+        double s_c;
+
+        bool Find_Same_SampleData(int num)
+        {
+            for(int i=0;i<N;i++)
+            {
+                if(sample_data[i].X == total_data[num].X && sample_data[i].Y == total_data[num].Y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void Get_Sample()
+        {
+
+            System.Array.Clear(sample_data,0,sample_data.Length);
+            int i = 0;
+            while(i != N)
+            {
+                int j = r.Next(0, total_data.Length);
+               
+                if (!Find_Same_SampleData(j))
+                {
+                    sample_data[i] = total_data[j];
+                    i++;
+                }
+            }
+        }
+
+        void make_pinv()
+        {
+            s_a = 0;
+            s_b = 0;
+            s_c = 0;
+
+            ma3 = new double[sample_data.Length, 3];
+            mb3 = new double[sample_data.Length, 1];
+            mabc3 = new double[3, 1];
+            mpa3 = new double[3, sample_data.Length];
+
+            for (int i = 0; i < sample_data.Length; i++)
+            {
+                ma3[i, 0] = sample_data[i].X * sample_data[i].X;
+                ma3[i, 1] = sample_data[i].X;
+                ma3[i, 2] = 1;
+                mb3[i, 0] = sample_data[i].Y;
+            }
+            MatrixA3 = new Mat(sample_data.Length, 3, MatType.CV_64FC1, ma3);
+            MatrixB3 = new Mat(sample_data.Length, 1, MatType.CV_64FC1, ma3);
+
+            Matrix_PinvA3 = new Mat(3, sample_data.Length, MatType.CV_64FC1, mpa3);
+            Cv2.Invert(MatrixA3, Matrix_PinvA3, DecompTypes.SVD);
+
+            for (int i = 0; i < inliers_count; i++)
+            {
+                s_a = s_a + Matrix_PinvA3.At<double>(0, i) * MatrixB3.At<double>(i, 0);
+                s_b = s_b + Matrix_PinvA3.At<double>(1, i) * MatrixB3.At<double>(i, 0);
+                s_c = s_c + Matrix_PinvA3.At<double>(2, i) * MatrixB3.At<double>(i, 0);
+            }
+        }
+
+        void compute_inlier()
+        {
+            inliers_count = 0;
+            for (int i = 0; i < max_cost; i++)
+            {
+                double y = s_a * i * i + s_b * i + s_c;
+                if (threshold >= Math.Abs((sample_data[i].Y) - y))
+                {
+                    inliers[inliers_count] = total_data[i];
+                    inliers_count++;
+                }
+            }
+        }
+        void compute_model()
+        {
+            cost = 0;
+            for (int i = 0; i < total_data.Length; i++)
+            {
+                double y = s_a * i * i + s_b * i + s_c;
+                if(threshold >= Math.Abs((total_data[i].Y)-y))
+                {
+                    cost++;
+                }
+            }
+        }
+    
+        public Form1()
+        {
+            InitializeComponent();   
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            chart1.Series.Clear();
+            chart1.Legends[0].Enabled = false;
+            chart1.ChartAreas[0].AxisX.Title = "X";      //chart를 통해 그래프를 그리기 위한 초기 세팅
+            chart1.ChartAreas[0].AxisY.Title = "Y";
+            i_value = Convert.ToInt32(richTextBox1.Text);
+
+            MatrixA = new double[i_value, 2];
+            MatrixB = new double[i_value, 1];
+            Pinv_MatrixA = new double[2, i_value];
+            Matrix_ab = new double[2, 1];
+            numx = new double[i_value, 1];
+            numy = new double[i_value, 1];
+            sum_a = 0;
+            sum_b = 0;
+
+            System.Array.Clear(numx, 0, i_value);
+            System.Array.Clear(numy, 0, i_value);
+            System.Array.Clear(MatrixA, 0, 2 * i_value);
+            System.Array.Clear(Pinv_MatrixA, 0, 2 * i_value);
+            System.Array.Clear(Matrix_ab, 0, 2);
+            System.Array.Clear(MatrixB, 0, i_value);
+
+            chart1.Series.Add("Data");
+            chart1.Series["Data"].ChartType = SeriesChartType.Point;
+
+            for (int i = 0; i < i_value; i++)
+            {
+                numx[i, 0] = r.Next(1, i_value);
+                numy[i, 0] = r.Next(1, i_value);
+                MatrixA[i, 0] = numx[i, 0];
+                MatrixA[i, 1] = 1;
+                chart1.Series["Data"].Points.AddXY(numx[i, 0], numy[i, 0]);
+                System.Threading.Thread.Sleep(3);
+            }
+
+            d_numx = new Mat(i_value, 1, MatType.CV_64FC1, numx);
+            d_numy = new Mat(i_value, 1, MatType.CV_64FC1, numy);
+            d_MatrixA = new Mat(i_value, 2, MatType.CV_64FC1, MatrixA);
+            d_Pinv_MatrixA = new Mat(2, i_value, MatType.CV_64FC1, Pinv_MatrixA);
+            d_MatrixB = new Mat(i_value, 1, MatType.CV_64FC1, MatrixB);
+
+
+            Cv2.Invert(d_MatrixA, d_Pinv_MatrixA, DecompTypes.SVD);
+
+            for (int i = 0; i < i_value; i++)
+            {
+                sum_a = sum_a + d_Pinv_MatrixA.At<double>(0, i) * d_numy.At<double>(i, 0);
+                sum_b = sum_b + d_Pinv_MatrixA.At<double>(1, i) * d_numy.At<double>(i, 0);
+
+            }
+
+            Matrix_ab[0, 0] = sum_a;
+            Matrix_ab[1, 0] = sum_b;
+            d_Matrix_ab = new Mat(2, 1, MatType.CV_64FC1, Matrix_ab);
+            chart1.Series.Add("LSM");
+            chart1.Series["LSM"].ChartType = SeriesChartType.Line;
+
+            for (int i = 0; i < i_value * 2; i++)
+            {
+                chart1.Series["LSM"].Points.AddXY(i, Matrix_ab[0, 0] * i + Matrix_ab[1, 0]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RanSeries chart2 = new RanSeries(0);
+            double[] xData;
+            double[] yData;
+            double[] zData;
+
+            i_value = Convert.ToInt32(richTextBox1.Text);
+
+            three_MatrixA = new double[i_value, 4];
+            three_Matrix_abcd = new double[4, 1];
+            s = new double[4, 1];
+            u = new double[i_value, 4];
+            v = new double[4, 4];
+            xData = new double[i_value];
+            yData = new double[i_value];
+            zData = new double[i_value];
+
+
+
+            for (int i = 0; i < i_value; i++)
+            {
+
+                three_MatrixA[i, 0] = r.Next(1, i_value);
+                xData[i] = three_MatrixA[i, 0];
+                three_MatrixA[i, 1] = r.Next(1, i_value);
+                yData[i] = three_MatrixA[i, 1];
+                three_MatrixA[i, 2] = r.Next(1, i_value);
+                zData[i] = three_MatrixA[i, 2];
+                three_MatrixA[i, 3] = 1;
+
+            }
+
+
+            ThreeDScatterChart c = new ThreeDScatterChart(360, 300);
+            c.addTitle("3D 분산 형 차트 (1)", "Times New Roman Italic", 2);
+            c.setPlotRegion(175, 140, 180, 180, 135);
+            c.addScatterGroup(xData, yData, zData, "", ChartDirector.Chart.GlassSphere2Shape, 11,
+            ChartDirector.Chart.SameAsMainColor);
+            winChartViewer1.Chart = c;
+
+
+            d_three_MatrixA = new Mat(i_value, 4, MatType.CV_64FC1, three_MatrixA);
+            S = new Mat(4, 1, MatType.CV_64FC1, s);
+            U = new Mat(i_value, 4, MatType.CV_64FC1, u);
+            V = new Mat(4, 4, MatType.CV_64FC1, v);
+
+
+            Cv2.SVDecomp(d_three_MatrixA, S, U, V);
+            three_Matrix_abcd[0, 0] = V.At<double>(0, 3);
+            three_Matrix_abcd[1, 0] = V.At<double>(1, 3);
+            three_Matrix_abcd[2, 0] = V.At<double>(2, 3);
+            three_Matrix_abcd[3, 0] = V.At<double>(3, 3);
+
+            double[] zzzzz = new double[yData.Length * xData.Length];
+            for (int i = 0; i < yData.Length; i++)
+            {
+                double yy = yData[i];
+                for (int j = 0; j < xData.Length; j++)
+                {
+                    double xx = xData[j];
+                    zzzzz[i * xData.Length + j] = (1 / three_Matrix_abcd[2, 0]) * (-(three_Matrix_abcd[0, 0] * xx) - (three_Matrix_abcd[0, 0] * yy) - three_Matrix_abcd[3, 0]);
+                }
+            }
+
+            SurfaceChart d = new SurfaceChart(360, 300);
+            d.setPlotRegion(175, 140, 180, 180, 135);
+            d.setData(xData, yData, zzzzz);
+            c.makeChart().merge(d.makeChart(), 0, 0, 0, 150);
+            winChartViewer1.Chart = c;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            chart1.Series.Clear();
+            chart1.Legends[0].Enabled = false;
+            chart1.ChartAreas[0].AxisX.Title = "X";      //chart를 통해 그래프를 그리기 위한 초기 세팅
+            chart1.ChartAreas[0].AxisY.Title = "Y";
+           
+            richTextBox2.Clear();
+            
+
+            i_value = Convert.ToInt32(richTextBox1.Text);
+            N = Convert.ToInt32(richTextBox3.Text);
+            threshold = Convert.ToDouble(richTextBox4.Text);
+
+            max_cost = 0;
+            max_rotation_count = Math.Log(1 - 0.99) / Math.Log(1 - Math.Pow(0.75, N));
+
+            
+            if (i_value < N)
+            {
+                return;
+            }
+
+            total_data = new OpenCvSharp.Point[i_value];
+            sample_data = new OpenCvSharp.Point[N];
+            inliers = new OpenCvSharp.Point[i_value];
+
+            chart1.Series.Add("something");
+            chart1.Series["something"].ChartType = SeriesChartType.Point;
+
+            for(int i=0;i<i_value;i++)
+            {
+                total_data[i].X = i;
+                total_data[i].Y = -(i * i) + 200;
+                if (i >= i_value / 3 && i <= (i_value / 3) + (i_value / 5))
+                {
+                    total_data[i].Y = total_data[i].Y + 1500;
+                }
+                chart1.Series["something"].Points.AddXY(total_data[i].X, total_data[i].Y);
+            }
+
+
+            for(int i=0;i<(int)max_rotation_count;i++)
+            {
+
+                Get_Sample();
+                make_pinv();
+                compute_model();
+
+                if(max_cost <cost)
+                {
+                    max_cost = cost;
+                   compute_inlier();
+                }
+            }
+
+            ma3 = new double[inliers_count, 3];
+            mb3 = new double[inliers_count,1];
+            mabc3 = new double[3,1];
+            mpa3 = new double[3, inliers_count];
+
+            for (int i = 0; i < inliers_count; i++)
+            {
+                ma3[i, 0] = inliers[i].X * inliers[i].X;
+                ma3[i, 1] = inliers[i].X;
+                ma3[i, 2] = 1;
+                mb3[i,0] = inliers[i].Y;
+            }
+            richTextBox2.AppendText("inlie_cnt: " + inliers_count + "\n");
+            richTextBox2.AppendText("inlie_cnt: " + max_cost + "\n");
+            MatrixA3 = new Mat(inliers_count, 3, MatType.CV_64FC1, ma3);
+            MatrixB3 = new Mat(inliers_count, 1, MatType.CV_64FC1, ma3);
+           
+            Matrix_PinvA3 = new Mat(3, inliers_count, MatType.CV_64FC1, mpa3);
+            Cv2.Invert(MatrixA3, Matrix_PinvA3,DecompTypes.SVD);
+
+            sum_a = 0;
+            sum_b = 0;
+            double sum_c = 0;
+
+            for (int i = 0; i < inliers_count; i++)
+            {
+                sum_a = sum_a + Matrix_PinvA3.At<double>(0, i) * MatrixB3.At<double>(i, 0);
+                sum_b = sum_b + Matrix_PinvA3.At<double>(1, i) * MatrixB3.At<double>(i, 0);
+                sum_c = sum_c + Matrix_PinvA3.At<double>(2, i) * MatrixB3.At<double>(i, 0);
+            }
+
+            mabc3[0, 0] = sum_a;
+            mabc3[1, 0] = sum_b;
+            mabc3[2, 0] = sum_c;
+            Matrix_abc3 = new Mat(3, 1, MatType.CV_64FC1,mabc3);
+
+            chart1.Series.Add("inliers");
+            chart1.Series["inliers"].ChartType = SeriesChartType.Spline;
+            
+            
+            for (int i=0;i<i_value;i++)
+            {
+                double y = (sum_a * i * i) + (sum_b * i) + sum_c;
+                chart1.Series["inliers"].Points.AddXY(i, y);
+
+            }
+
+        }
+
+       
+
+       
+    }
+}
